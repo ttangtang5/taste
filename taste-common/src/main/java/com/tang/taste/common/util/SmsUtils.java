532eb186ecc3e623c926e1c4902dd6bf1b0d8859
ttangtang5 <34825640+ttangtang5@users.noreply.github.com>
2020-02-28 11:23:17 +0800

    Update SmsUtils.java

--- taste-common/src/main/java/com/tang/taste/common/util/SmsUtils.java
+++ taste-common/src/main/java/com/tang/taste/common/util/SmsUtils.java
@@ -42,8 +42,8 @@ public class SmsUtils {
     static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAITPrlCEQyTgt2";
-    static final String accessKeySecret = "FXjHq9sCPNXcDXlSzUA0kE8KoBjNGh";
+    static final String accessKeyId = "";
+    static final String accessKeySecret = "";
 
     /**
      * 发送短信  验证码类型
@@ -68,7 +68,7 @@ public class SmsUtils {
         //必填:待发送手机号
         request.setPhoneNumbers(phoneNum);
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("唐蓝云");
+        request.setSignName("tang");
         //必填:短信模板-可在短信控制台中找到
         request.setTemplateCode(template);
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
@@ -115,7 +115,7 @@ public class SmsUtils {
         //必填:待发送手机号
         request.setPhoneNumbers(phoneNum);
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("唐蓝云");
+        request.setSignName("tang");
         //必填:短信模板-可在短信控制台中找到
         request.setTemplateCode(template);
         booking.setTimeStr(DateUtil.getDateTime("yyyy-MM-dd HH:mm:ss",booking.getTime()));
@@ -164,7 +164,7 @@ public class SmsUtils {
         //必填:待发送手机号
         request.setPhoneNumbers(phoneNum);
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("唐蓝云");
+        request.setSignName("tang");
         //必填:短信模板-可在短信控制台中找到
         request.setTemplateCode(template);
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
